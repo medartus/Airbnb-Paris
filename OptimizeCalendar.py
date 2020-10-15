14881529f951317362535e2d58400fcfdc7f0f6b
Marc-Etienne Dartus <marcetienne.dartus@gmail.com>
2020-10-15 09:28:20 +0200

    Adding Docker

---
+++ OptimizeCalendar.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+
+def groupDate(group):
+    return [group.iloc[0]['date'], group.iloc[-1]['date'], str(group.iloc[0]['listing_id']),  group.iloc[0]['available'],group.shape[0],  group.iloc[0]['minimum_nights'],  group.iloc[0]['maximum_nights']]
+
+def OptimizeCalendar(filename):
+    calendar = pd.read_csv(filename,sep=",")
+
+    calendar = calendar.sort_values(by=["listing_id", "date"])
+    calendar = calendar.reset_index()
+
+    del calendar['price']
+    del calendar['index']
+    del calendar['adjusted_price']
+
+    adj_check = (calendar.available != calendar.available.shift()).cumsum()
+    newData = pd.DataFrame(calendar.groupby(['listing_id','available',adj_check], as_index=False, sort=False).apply(groupDate))
+    newData[['start','end','listing_id','available','num_day','minimum_nights','maximum_nights']] = pd.DataFrame(newData[0].to_list(), index= newData.index)
+    newData["start"] = pd.to_datetime(newData["start"])
+    newData["end"] = pd.to_datetime(newData["end"])
+    
+    del newData[0]
+    newData = newData.reset_index(drop=True)
+    
+    return newData
+
+newData = OptimizeCalendar("../datasets/calendar08.csv")
+
+# import mysql.connector
+
+# cnx = mysql.connector.connect(user='user', password='password',
+#                               host='127.0.0.1',
+#                               database='airbnb')
+# cursor = cnx.cursor()
+
+# query = ("SELECT * from plop")
+
+# cursor.execute(query)
+
+# for val in cursor:
+#   print(val)
+
+# cursor.close()
+# cnx.close()
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-Airbnb
+# Airbnb - Paris
+## Getting started
+1. Install python modules
+```
+pip install -r requirements.txt
+```
+2. Start Mysql database
+```
+docker-compose up
+```
+
+Database informations :
+- MYSQL_DATABASE: `airbnb`
+- MYSQL_USER: `user`
+- MYSQL_PASSWORD: `password`
+- MYSQL_ROOT_PASSWORD: `password`
